aece1444c147265b3d50bb5d1f4f3d68a26dc0b8
Brian Hie <brianhie@mit.edu>
2018-11-28 14:18:35 -0500

    update tar

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.6',
     description='Panoramic stitching of heterogeneous single cell transcriptomic data',
     url='https://github.com/brianhie/scanorama',
-    download_url='https://github.com/brianhie/scanorama/archive/v0.5.tar.gz',
+    download_url='https://github.com/brianhie/scanorama/archive/v0.6.tar.gz',
     packages=find_packages(exclude=['bin', 'conf', 'data', 'target']),
     install_requires=[
         'annoy>=1.11.5',
