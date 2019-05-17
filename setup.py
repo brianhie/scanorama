26d60b40bc88b0dedb480a3bff4fa9fb65e238f0
Brian Hie <brianhie@mit.edu>
2019-05-17 18:05:38 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='scanorama',
-    version='1.0',
+    version='1.1',
     description='Panoramic stitching of heterogeneous single cell transcriptomic data',
     url='https://github.com/brianhie/scanorama',
-    download_url='https://github.com/brianhie/scanorama/archive/v1.0.tar.gz',
+    download_url='https://github.com/brianhie/scanorama/archive/v1.1.tar.gz',
     packages=find_packages(exclude=['bin', 'conf', 'data', 'target']),
     install_requires=[
         'annoy>=1.11.5',
